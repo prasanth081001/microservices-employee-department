f8506ed8e22bc7d15a7cea0b5c4e8933f79ebf70
PRASANTH <https://github.com/prasanth081001/StudentAttendanceMark.git@>
2026-08-27 12:30:58 +0530

    Day 8 : Implement Retry and circuit breaker -Resilienceg4j

--- employee-service/src/main/java/com/example/employee_service/Config/FeignRetryConfig.java
+++ employee-service/src/main/java/com/example/employee_service/Config/FeignRetryConfig.java
@@ -8,11 +8,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class FeignRetryConfig {
     @Bean
-    public Retryer feignRetryer(){
-        return new Retryer.Default(100,1000,2){
+    public Retryer feignRetry() {
+
+        System.out.println("========== CUSTOM FEIGN RETRYER CREATED ==========");
+
+        return new Retryer.Default(100, 1000, 3) {
+
             @Override
-            public void continueOrPropagate(RetryableException e){
-                System.out.println("Feign Retry :" +e.getMessage());
+            public void continueOrPropagate(RetryableException e) {
+
+                System.out.println("========== FEIGN RETRY ==========");
+                System.out.println("Retrying: " + e.getMessage());
+
                 super.continueOrPropagate(e);
             }
         };
